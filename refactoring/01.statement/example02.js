ce5824cd5500814557f8ffb15d70ea9ef1975433
malrang <seniya2@gmail.com>
2022-11-17 16:45:34 +0900

    feat: update

---
+++ refactoring/01.statement/example02.js
@@ -0,0 +1,82 @@
+const plays = {
+  'hamlet': {'name': 'Hamlet', 'type': 'tragedy'},
+  'as-like': {'name': 'As you Like It', 'type': 'comedy'},
+  'othello': {'name': 'Othello', 'type': 'tragedy'}
+}
+
+const invoices = [
+  {
+    'customer': 'BigCo',
+    'performances': [
+      {
+        'playID': 'hamlet',
+        'audience': 55
+      },
+      {
+        'playID': 'as-like',
+        'audience': 35
+      },
+      {
+        'playID': 'othello',
+        'audience': 40
+      },
+    ]
+  }
+]
+
+
+
+function statement(invoice, plays) {
+  let totalAmount = 0
+  let volumeCredits = 0
+  let result = `청구 내역 (고객명: ${invoice.customer})\n`
+  const format = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD', minimumFractionDigits: 2}).format
+
+  function amountFor(aPerformance, play) {
+    let result = 0
+  
+    switch (play.type) {
+      case 'tragedy': // 비극
+        result = 40000
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30)
+        }
+        break;
+  
+      case 'comedy':  // 희극
+        result = 30000
+        if (aPerformance.audience > 20) {
+          result += 1000 + 500 * (aPerformance.audience - 20)
+        }
+        result += 300 * aPerformance.audience      
+        break;
+    
+      default:
+        throw new Error('알 수 없는 장르: ${play.type}')
+        break;
+    }
+    return result
+  }
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID]
+    let thisAmount = amountFor(perf, play)
+
+    // 포인트를 적립한다.
+    volumeCredits += Math.max(perf.audience - 30, 0)
+
+    // 희극 관객 5명 마다 추가 포인트를 제공한다
+    if ('comedy' === play.type) volumeCredits += Math.floor(perf.audience / 5)
+
+    // 청구 내역을 출력한다
+    result += ` ${play.name}: ${format(thisAmount/100)} (${perf.audience})석\n`
+    totalAmount += thisAmount
+  }
+
+  result += `총액: ${format(totalAmount/100)}\n`
+  result += `적립 포인트: ${volumeCredits}점\n`
+  return result
+}
+
+const statementResult = statement(invoices[0], plays)
+console.log(statementResult)
